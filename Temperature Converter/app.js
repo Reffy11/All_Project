1139d203af7cc09102c3d89abcfa3fc5cbf77838
Shriya Naik <shreyanaik527@gmail.com>
2023-06-20 16:04:36 +0530

    Add files via upload

---
+++ Temperature Converter/app.js
@@ -0,0 +1,16 @@
+function converttemperature(){
+    var temperature=document.getElementById('temperature').value;
+    var unit= document.getElementById('unit').value;
+
+if (unit== 'farenheit')
+{
+ var result=(temperature-32)*(5/9);
+ result = result.toFixed(2);
+ result = result + '℃'
+}else{
+    var result = (temperature*(9/5))+32;
+    result = result.toFixed(2);
+    result = result + '℉'
+}
+document.getElementById('result').innerHTML= result;
+}
